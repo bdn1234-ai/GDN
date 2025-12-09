5b854fe89e045bf23e6d07263d7540e0669234b9
bdn1234-ai <doanbao033@gmail.com>
2025-12-09 09:56:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
-pip install --no-index torch-scatter -f https://pytorch-geometric.com/whl/torch-1.5.1+cu102.html
-pip install --no-index torch-sparse -f https://pytorch-geometric.com/whl/torch-1.5.1+cu102.html
-pip install --no-index torch-cluster -f https://pytorch-geometric.com/whl/torch-1.5.1+cu102.html
-pip install --no-index torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.5.1+cu102.html
-pip install torch-geometric==1.5.0
+# Cài đặt các thư viện phụ trợ cho PyTorch 1.5.1 + CUDA 10.2
+!pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.5.1+cu102.html
+!pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.5.1+cu102.html
+!pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.5.1+cu102.html
+!pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.5.1+cu102.html
+
+# Cài đặt torch-geometric đúng phiên bản dự án yêu cầu
+!pip install torch-geometric==1.5.0
